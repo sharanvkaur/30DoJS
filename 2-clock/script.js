09e7d5bf4182cc133fba98d3d0564040ab9641c1
Sharan Kaur <sharanvkaur@gmail.com>
2016-12-25 00:19:20 +0800

    :tada:

---
+++ 2-clock/script.js
@@ -0,0 +1,18 @@
+function setDate() {
+  const now = new Date()
+  console.log(now);
+  const seconds = now.getSeconds()
+  const secDeg = ((seconds/60) * 360) + 90
+  const secHand = document.querySelector('.secs-hand')
+  secHand.style.transform = `rotate(${secDeg}deg)`
+
+  const minutes = now.getMinutes()
+  const minDeg = ((minutes/60) * 360) + 90
+  const minHand = document.querySelector('.mins-hand')
+  minHand.style.transform = `rotate(${minDeg}deg)`
+
+  const hourDeg = (minutes/2) + 90
+  const hourHand = document.querySelector('.hour-hand')
+  hourHand.style.transform = `rotate(${hourDeg}deg)`
+}
+setInterval(setDate, 1000)
