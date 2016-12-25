9ee7f511508f6506c4bf759ff2be0417209de687
Sharan Kaur <sharanvkaur@gmail.com>
2016-12-25 16:54:36 +0800

    :tada:

--- 2-clock/script.js
+++ 2-clock/script.js
@@ -11,8 +11,9 @@ function setDate() {
   const minHand = document.querySelector('.mins-hand')
   minHand.style.transform = `rotate(${minDeg}deg)`
 
-  const hourDeg = (minutes/2) + 90
+  const hourDeg = ((minutes/12) * 360) + 90
   const hourHand = document.querySelector('.hour-hand')
   hourHand.style.transform = `rotate(${hourDeg}deg)`
+  console.log(hourDeg);
 }
 setInterval(setDate, 1000)
